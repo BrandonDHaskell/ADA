1d0bd3cbfc4afc73d4ee51ead3c27fb0f02d7660
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-13 08:27:18 -0800

    update hardware_interface logging

--- src/hardware_interface.py
+++ src/hardware_interface.py
@@ -1,3 +1,4 @@
+import logging
 from abc import ABC, abstractmethod
 
 """
@@ -5,10 +6,11 @@ An abstract class base for all hardware components in the ADA system.
 This should be the basic methods that all hardware uses with ADA.
 """
 class HardwareInterface(ABC):
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
     """
     Initialize the hardware. This might be just setting the GPIO pins to use.
     """
     @abstractmethod
     def initialize(self):
-        pass
-    
+        self.logger.info("Initializing hardware")
