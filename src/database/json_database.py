9bcde2ce3845a72fac9d846d368f2e80e6c46ac8
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-14 10:28:21 -0800

    refactor json_database.py to return member_info object, add some data validation

--- src/database/json_database.py
+++ src/database/json_database.py
@@ -41,11 +41,26 @@ class JsonDatabase(DatabaseInterface):
         except Exception as e:
             self.logger.error(f"Error saving data to {self.filepath}: {e}")
 
-    def add_member(self, obf_rfid, member_info):
+    def _validate_member_info(self, member_info):
+        """
+        Validate that member_info contains an obfuscated RFID.
+        If not, raise a ValueError
+        """
+        obf_rfid = member_info.get("obf_rfid")
+        if not obf_rfid:
+            raise ValueError("Member RFID is required")
+        return obf_rfid
+
+    def add_member(self, member_info):
         """
         Add a new member to the database
         Raises ValueError if member already exists in database
         """
+
+        # validate an obfuscated RFID is in member_info
+        obf_rfid = self._validate_member_info(member_info)
+        
+        # validate obfuscated RFID is unique
         if obf_rfid in self.data:
             self.logger.error(f"Member with ID {obf_rfid} already exists in database")
             raise ValueError(f"Member with RFID {obf_rfid} already exists")
@@ -53,25 +68,39 @@ class JsonDatabase(DatabaseInterface):
         self.data[obf_rfid] = member_info
         self._save_data()
         self.logger.info(f"Member added with RFID {obf_rfid}")
+        return member_info
 
-    def get_member(self, obf_rfid):
+    def get_member(self, member_info):
         """
         Retrieve a member's details from the database
         Follows python's dictionary return results
         """
-        member = self.data.get(obf_rfid)
-        if member:
+
+        # validate an obfuscated RFID is in member_info
+        obf_rfid = self._validate_member_info(member_info)
+        
+        # get member_info from obfuscated RFID
+        member_info = self.data.get(obf_rfid)
+        if member_info:
             self.logger.info(f"Retrieved member with ID {obf_rfid}")
         else:
             self.logger.warn(f"Member with ID {obf_rfid} not found")
-        return member
+        return member_info
     
-    def update_member(self, obf_rfid, member_info):
+    def update_member(self, member_info):
         """
         Update a member's record in the database.
         """
+
+        # validate an obfuscated RFID is in member_info
+        obf_rfid = self._validate_member_info(member_info)
+        
+        # validate an obfuscated RFID is in database
         if obf_rfid not in self.data:
             self.logger.error(f"Cannot update: Member with ID {obf_rfid} does not exist in the database")
             raise KeyError(f"Member with ID {obf_rfid} not found")
         
         self.data[obf_rfid].update(member_info)
+        self._save_data()
+        self.logger.info(f"Member with RFID {obf_rfid} updated.")
+        return self.data[obf_rfid]
