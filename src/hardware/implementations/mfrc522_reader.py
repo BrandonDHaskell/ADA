fdb90996870ab481b1795d170fe4d875ebfd0b90
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-15 22:55:42 -0800

    reorder __init__ flow

--- src/hardware/implementations/mfrc522_reader.py
+++ src/hardware/implementations/mfrc522_reader.py
@@ -10,8 +10,8 @@ class MFRC522Reader(RFIDScanner):
     Raspberry Pi GPIO.
     """
     def __init__(self, config):
-        super().__init__(config)
         self.config = config
+        super().__init__(config)
         self.reader = BasicMFRC522()
         self.initialize()
 
