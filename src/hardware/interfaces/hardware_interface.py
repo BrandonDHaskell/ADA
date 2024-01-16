abffda6fc4421c9b79b3730f6e9274385ea1b211
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-15 20:05:53 -0800

    update hardware_interface to require name for logging and require config

--- src/hardware/interfaces/hardware_interface.py
+++ src/hardware/interfaces/hardware_interface.py
@@ -6,11 +6,18 @@ An abstract class base for all hardware components in the ADA system.
 This should be the basic methods that all hardware uses with ADA.
 """
 class HardwareInterface(ABC):
-    def __init__(self):
-        self.logger = logging.getLogger(self.__class__.__name__)
+    def __init__(self, config):
+        if not config.get("name"):
+            raise ValueError("A 'name' must be provided in the config")
+        
+        self.name = config["name"]
+        self.logger = logging.getLogger(self.name)
+
+        self.initialize()
+        
     """
     Initialize the hardware. This might be just setting the GPIO pins to use.
     """
     @abstractmethod
-    def initialize(self, config_info):
+    def initialize(self):
         self.logger.info("Initializing hardware")
