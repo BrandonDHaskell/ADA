a9522f96d1404dca96532b8bdca87d6d35b9c216
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-16 22:14:32 -0800

    update database_interface.py to interface with ada_interface

--- src/database/interfaces/database_interface.py
+++ src/database/interfaces/database_interface.py
@@ -1,26 +1,26 @@
-import logging
-from abc import ABC, abstractmethod
+from abc import abstractmethod
+from ada_interface import ADAInterface
 
-class DatabaseInterface(ABC):
+class DatabaseInterface(ADAInterface):
     """
     Abstract base class for database interactions for the ADA system.
     This interface is designed to be adaptable to various database backends.
     """
 
-    def __init__(self, connection_info):
-        self.logger = logging.getLogger(self.__class__.__name__)
-        self.connection_info = connection_info
-        self.logger.info(f"Initializing database with connection info: {connection_info}")
-        self.initialize(connection_info)
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
+        self.connection_info = config["connection_info"]
+        self.initialize()
 
     @abstractmethod
-    def initialize(self, connection_info):
+    def initialize(self):
         """
         Initialize the database connection.
         Implementations should log the successful initialization or any errors.
         :param connection_info: Information required to establish a connection to the database.
         """
-        pass
+        self.logger.debug(f"Initializing  with config info: {self.config}")
 
     @abstractmethod
     def add_member(self, obf_rfid, member_info):
