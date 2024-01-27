0c5d2130fbb0627d3337b755ed28659f4d94b476
BrandonDHaskell <BrandonDHaskell@outlook.com>
2024-01-27 09:50:47 -0800

    update member_schema and add notes

--- src/schemas/member_schema.py
+++ src/schemas/member_schema.py
@@ -1,7 +1,9 @@
 member_schema = {
-    "obf_rfid": str,
-    "member_level": str,
-    "membership_status": str,
-    "access_interval": str,
-    "member_sponsor": str
+    "obf_rfid": str,            # obfuscated RFID
+    "member_level": str,        # determined member level: 'guest', 'member', 'admin'
+    "membership_status": str,   # determines member status: 'active' or 'inactive'
+    "access_interval": str,     # determines temporary access interval (follows ISO 8601 repeating interval format)
+    "member_sponsor": str,      # member who sponsored this member (their obfuscated RFID)
+    "created": str,             # Read only, should be set by the DB (follows ISO 8601 timestamp format)
+    "last_updated": str         # Read only, should be set by the DB (follows ISO 8601 timestamp format)
 }
